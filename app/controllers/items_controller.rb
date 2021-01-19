7751862d75e095c31c6bdac6c3013c762dca6844
satoshi-takamatsu <70362048+satoshi-takamatsu@users.noreply.github.com>
2021-01-19 16:06:21 +0900

    商品情報編集機能の実装コードレビュー修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit]
   before_action :authenticate_user!, only: [:new, :edit]
   
   def index
@@ -18,18 +19,16 @@ class ItemsController < ApplicationController
     end
   end
 
+  # 記憶の定着の為 before_actionを使いインスタンス変数の定義をまとめる
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-
     unless @item.user_id == current_user.id
       redirect_to root_path
     end
-    
   end
+    # /記憶の定着の為 before_actionを使いインスタンス変数の定義をまとめる
 
   def update
     @item = Item.find(params[:id])
@@ -46,10 +45,16 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :description, :price, :image, :category_id, :state_id, :delivery_fee_id, :shipping_area_id, :day_to_ship_id).merge(user_id: current_user.id)
   end
 
-  def authenticate_user!
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
+  def set_item
+    @item = Item.find(params[:id])
   end
+
+  # 記憶の定着として残しております。 
+  # authenticate_user! deviseのヘルパーメソッドで定義されている、下記の記述はいらない。before_actionに指定すればOK
+  # def authenticate_user!
+  #   unless user_signed_in?
+  #     redirect_to new_user_session_path
+  #   end
+  # end
 
 end
